9ae022f0b5178f312491c752a5f2ad708c6b3cc9
borhanrahi <borhanrahi111@gmail.com>
2024-10-09 17:05:01 +0600

    update route

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET(request: Request) {
+export async function GET() {
   const supabase = createRouteHandlerClient({ cookies });
 
   try {
